e58a3310f358a8b1b6f9e0dcab1cb1aea83e06b3
Thong Zi Qi <ziqi.thong@gamil.com>
2025-02-16 15:59:26 +0800

    chores - forward declaration

--- srcs/Channel/Channel.hpp
+++ srcs/Channel/Channel.hpp
@@ -2,7 +2,6 @@
 
 # include "ircserv.hpp"
 
-<<<<<<< HEAD
 class IObserver;
 
 class ISubject {
@@ -13,26 +12,16 @@ class ISubject {
         virtual void    notify(const std::string& message) = 0;
         virtual string  getName() const = 0;
         virtual bool    isClientExist(const int client_fd) = 0;
-=======
-class ISubject {
-    public:
-        virtual void attach(int client_fd, IObserver* client) = 0;
-        virtual void detach(int client_fd) = 0;
-        virtual void notify(const std::string& message) = 0;
-        virtual ~ISubject() {}
->>>>>>> c78f6ca (feat - Subject Interface and concrete Channel Class)
 };
+
 
 namespace IRC
 {
     class Channel: public ISubject
     {
         private:
-<<<<<<< HEAD
 			string						_channel_name;
-=======
 			string						_channelName;
->>>>>>> c78f6ca (feat - Subject Interface and concrete Channel Class)
 			std::map<int, IObserver*>	_clients; /**<client_fd, Client*> */
         public:
             Channel();
@@ -40,14 +29,8 @@ namespace IRC
             Channel(const string channel_name);
             Channel(const Channel &other);
             Channel &operator=(const Channel &other);
-
-<<<<<<< HEAD
-            void	attach(IObserver* client);
-			void	detach(IObserver* client);
-=======
             void	attach(int client_fd, IObserver* client);
 			void	detach(int client_fd);
->>>>>>> c78f6ca (feat - Subject Interface and concrete Channel Class)
 			void	notify(const std::string& message);
 			void	sendMessage(const IObserver* sender, const string& msg);
 
@@ -55,11 +38,6 @@ namespace IRC
 			void	setChannelName(const string& channel_name);
 
             //Getter(s)
-<<<<<<< HEAD
-            string  getName() const;
-            bool    isClientExist(const int client_fd);
-=======
             string    getChannelName() const;
->>>>>>> c78f6ca (feat - Subject Interface and concrete Channel Class)
     };
 }
