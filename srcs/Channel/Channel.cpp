f3f65694013624f267220d3eb1ac662f767e9840
Thong Zi Qi <ziqi.thong@gamil.com>
2025-02-16 15:42:44 +0800

    feat - Subject Interface and concrete Channel Class

--- srcs/Channel/Channel.cpp
+++ srcs/Channel/Channel.cpp
@@ -4,19 +4,27 @@ IRC::Channel::Channel() {}
 
 IRC::Channel::~Channel() {}
 
+<<<<<<< HEAD
 IRC::Channel::Channel(const string channel_name): _channel_name(channel_name) {}
 
+=======
+>>>>>>> c78f6ca (feat - Subject Interface and concrete Channel Class)
 IRC::Channel::Channel(const Channel &other) { *this = other; }
 
 IRC::Channel& IRC::Channel::operator=(const Channel &other)
 {
 	if (this != &other)
+<<<<<<< HEAD
         this->_channel_name = other._channel_name;
+=======
+        this->_channelName = other._channelName;
+>>>>>>> c78f6ca (feat - Subject Interface and concrete Channel Class)
         this->_clients = other._clients;
     return *this;
 }
 
 //ISubject Functions
+<<<<<<< HEAD
 void	IRC::Channel::attach(IObserver* client)
 {
 	int client_fd = client->getClientFd();
@@ -24,20 +32,38 @@ void	IRC::Channel::attach(IObserver* client)
 
 	if (this->_clients.find(client_fd) != this->_clients.end())
 		logManager->logMsg(RED, (client->getNickname() + " already exist in channel " + this->_channel_name).c_str(), strerror(errno));
+=======
+void	IRC::Channel::attach(int client_fd, IObserver* client)
+{
+	IRC::Logger* logManager;
+
+	if (this->_clients.find(client_fd) != this->_clients.end())
+		logManager->logMsg(RED, (client_fd + " already exist in channel " + this->_channelName).c_str(), strerror(errno));
+>>>>>>> c78f6ca (feat - Subject Interface and concrete Channel Class)
 	else
 		this->_clients[client_fd] = client;
 }
 
+<<<<<<< HEAD
 void	IRC::Channel::detach(IObserver* client)
 {
 	int	client_fd = client->getClientFd();
 	IRC::Logger* logManager = IRC::Logger::getInstance();
+=======
+void	IRC::Channel::detach(int client_fd)
+{
+	IRC::Logger* logManager;
+>>>>>>> c78f6ca (feat - Subject Interface and concrete Channel Class)
 
 	std::map<int, IObserver*>::iterator it = this->_clients.find(client_fd);
 	if (it != this->_clients.end())
 		this->_clients.erase(it);
 	else
+<<<<<<< HEAD
 		logManager->logMsg(RED, (client->getNickname() + " deosn't exist in channel " + this->_channel_name).c_str(), strerror(errno));
+=======
+		logManager->logMsg(RED, (client_fd + " deosn't exist in channel " + this->_channelName).c_str(), strerror(errno));
+>>>>>>> c78f6ca (feat - Subject Interface and concrete Channel Class)
 }
 
 void	IRC::Channel::notify(const std::string& message)
@@ -50,6 +76,7 @@ void	IRC::Channel::notify(const std::string& message)
 
 void	IRC::Channel::sendMessage(const IObserver* sender, const string& msg)
 {
+<<<<<<< HEAD
 	this->notify("[" + sender->getNickname() + "] " + msg);
 }
 
@@ -67,3 +94,13 @@ bool	IRC::Channel::isClientExist(const int client_fd)
 		return true;
 	return false;
 }
+=======
+	this->notify("[" + sender->getName() + "] " + msg);
+}
+
+//Setter(s)
+void	IRC::Channel::setChannelName(const string& channel_name) { this->_channelName = channel_name; }
+
+//Getter(s)
+string	IRC::Channel::getChannelName() const { return this->_channelName; }
+>>>>>>> c78f6ca (feat - Subject Interface and concrete Channel Class)
--- srcs/Channel/Channel.hpp
+++ srcs/Channel/Channel.hpp
@@ -2,6 +2,7 @@
 
 # include "ircserv.hpp"
 
+<<<<<<< HEAD
 class IObserver;
 
 class ISubject {
@@ -12,6 +13,14 @@ class ISubject {
         virtual void    notify(const std::string& message) = 0;
         virtual string  getName() const = 0;
         virtual bool    isClientExist(const int client_fd) = 0;
+=======
+class ISubject {
+    public:
+        virtual void attach(int client_fd, IObserver* client) = 0;
+        virtual void detach(int client_fd) = 0;
+        virtual void notify(const std::string& message) = 0;
+        virtual ~ISubject() {}
+>>>>>>> c78f6ca (feat - Subject Interface and concrete Channel Class)
 };
 
 namespace IRC
@@ -19,7 +28,11 @@ namespace IRC
     class Channel: public ISubject
     {
         private:
+<<<<<<< HEAD
 			string						_channel_name;
+=======
+			string						_channelName;
+>>>>>>> c78f6ca (feat - Subject Interface and concrete Channel Class)
 			std::map<int, IObserver*>	_clients; /**<client_fd, Client*> */
         public:
             Channel();
@@ -28,8 +41,13 @@ namespace IRC
             Channel(const Channel &other);
             Channel &operator=(const Channel &other);
 
+<<<<<<< HEAD
             void	attach(IObserver* client);
 			void	detach(IObserver* client);
+=======
+            void	attach(int client_fd, IObserver* client);
+			void	detach(int client_fd);
+>>>>>>> c78f6ca (feat - Subject Interface and concrete Channel Class)
 			void	notify(const std::string& message);
 			void	sendMessage(const IObserver* sender, const string& msg);
 
@@ -37,7 +55,11 @@ namespace IRC
 			void	setChannelName(const string& channel_name);
 
             //Getter(s)
+<<<<<<< HEAD
             string  getName() const;
             bool    isClientExist(const int client_fd);
+=======
+            string    getChannelName() const;
+>>>>>>> c78f6ca (feat - Subject Interface and concrete Channel Class)
     };
 }
