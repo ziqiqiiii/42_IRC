ffa80e83d78e23dbe8313dd20f3e5d71cbec19ec
Thong Zi Qi <ziqi.thong@gamil.com>
2025-02-16 15:42:44 +0800

    feat - Subject Interface and concrete Channel Class

--- srcs/Channel/Channel.cpp
+++ srcs/Channel/Channel.cpp
@@ -1,1 +1,56 @@
 # include "Channel.hpp"
+
+IRC::Channel::Channel() {}
+
+IRC::Channel::~Channel() {}
+
+IRC::Channel::Channel(const Channel &other) { *this = other; }
+
+IRC::Channel& IRC::Channel::operator=(const Channel &other)
+{
+	if (this != &other)
+        this->_channelName = other._channelName;
+        this->_clients = other._clients;
+    return *this;
+}
+
+//ISubject Functions
+void	IRC::Channel::attach(int client_fd, IObserver* client)
+{
+	IRC::Logger* logManager;
+
+	if (this->_clients.find(client_fd) != this->_clients.end())
+		logManager->logMsg(RED, (client_fd + " already exist in channel " + this->_channelName).c_str(), strerror(errno));
+	else
+		this->_clients[client_fd] = client;
+}
+
+void	IRC::Channel::detach(int client_fd)
+{
+	IRC::Logger* logManager;
+
+	std::map<int, IObserver*>::iterator it = this->_clients.find(client_fd);
+	if (it != this->_clients.end())
+		this->_clients.erase(it);
+	else
+		logManager->logMsg(RED, (client_fd + " deosn't exist in channel " + this->_channelName).c_str(), strerror(errno));
+}
+
+void	IRC::Channel::notify(const std::string& message)
+{
+	std::map<int, IObserver*>::iterator	it;
+
+	for (it = this->_clients.begin(); it != this->_clients.end(); ++it)
+		it->second->update(message);
+}
+
+void	IRC::Channel::sendMessage(const IObserver* sender, const string& msg)
+{
+	this->notify("[" + sender->getName() + "] " + msg);
+}
+
+//Setter(s)
+void	IRC::Channel::setChannelName(const string& channel_name) { this->_channelName = channel_name; }
+
+//Getter(s)
+string	IRC::Channel::getChannelName() const { return this->_channelName; }
--- srcs/Channel/Channel.hpp
+++ srcs/Channel/Channel.hpp
@@ -2,16 +2,36 @@
 
 # include "IrcServ.hpp"
 
+class ISubject {
+    public:
+        virtual void attach(int client_fd, IObserver* client) = 0;
+        virtual void detach(int client_fd) = 0;
+        virtual void notify(const std::string& message) = 0;
+        virtual ~ISubject() {}
+};
+
 namespace IRC
 {
-    class Channel: public IObserver
+    class Channel: public ISubject
     {
         private:
-
+			string						_channelName;
+			std::map<int, IObserver*>	_clients; /**<client_fd, Client*> */
         public:
             Channel();
             ~Channel();
             Channel(const Channel &other);
             Channel &operator=(const Channel &other);
+
+            void	attach(int client_fd, IObserver* client);
+			void	detach(int client_fd);
+			void	notify(const std::string& message);
+			void	sendMessage(const IObserver* sender, const string& msg);
+
+			//Setter(s)
+			void	setChannelName(const string& channel_name);
+
+            //Getter(s)
+            string    getChannelName() const;
     };
 }
