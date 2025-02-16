b00e8f19dc2140a38ccedc764106501b4548d47c
Thong Zi Qi <ziqi.thong@gamil.com>
2025-02-16 19:27:03 +0800

    chores - rename private vairable and slightly modify Observer Interface

--- srcs/Channel/Channel.hpp
+++ srcs/Channel/Channel.hpp
@@ -7,10 +7,11 @@ class IObserver;
 class ISubject {
     public:
         virtual ~ISubject() {}
-        virtual void    attach(int client_fd, IObserver* client) = 0;
-        virtual void    detach(int client_fd) = 0;
+        virtual void    attach(IObserver* client) = 0;
+        virtual void    detach(IObserver* client) = 0;
         virtual void    notify(const std::string& message) = 0;
         virtual string  getName() const = 0;
+        virtual bool    isClientExist(const int client_fd) = 0;
 };
 
 namespace IRC
@@ -18,16 +19,17 @@ namespace IRC
     class Channel: public ISubject
     {
         private:
-			string						_channelName;
+			string						_channel_name;
 			std::map<int, IObserver*>	_clients; /**<client_fd, Client*> */
         public:
             Channel();
             ~Channel();
+            Channel(const string channel_name);
             Channel(const Channel &other);
             Channel &operator=(const Channel &other);
 
-            void	attach(int client_fd, IObserver* client);
-			void	detach(int client_fd);
+            void	attach(IObserver* client);
+			void	detach(IObserver* client);
 			void	notify(const std::string& message);
 			void	sendMessage(const IObserver* sender, const string& msg);
 
@@ -36,5 +38,6 @@ namespace IRC
 
             //Getter(s)
             string  getName() const;
+            bool    isClientExist(const int client_fd);
     };
 }
