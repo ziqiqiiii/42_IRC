827de8bae6c63ed121de85187020406f8ff4a4e9
Thong Zi Qi <ziqi.thong@gamil.com>
2025-03-15 02:15:20 +0800

    feat - splitString Utils function

--- srcs/Utils/Utils.cpp
+++ srcs/Utils/Utils.cpp
@@ -36,3 +36,30 @@ size_t	IRC::Utils::find_crlf(string &str)
 	}
 	return (0);
 }
+
+/**
+ * @brief Splits a string into a vector of substrings based on a specified delimiter.
+ *
+ * This method takes an input string and a delimiter, then splits the string into
+ * substrings wherever the delimiter occurs. The substrings are returned as a vector.
+ *
+ * @param s The input string to split.
+ * @param del The delimiter used to split the string. Defaults to a single space (" ").
+ * @return std::vector<string> A vector containing the split substrings.
+ */
+std::vector<string> IRC::Utils::splitString(const string& s, const string& del)
+{
+    std::vector<string> tokens;
+    size_t start = 0;
+    size_t end = s.find(del);
+
+    while (end != string::npos) {
+        tokens.push_back(s.substr(start, end - start));
+        start = end + del.size();
+        end = s.find(del, start);
+    }
+	// Add the last token
+    tokens.push_back(s.substr(start, end - start));
+
+    return tokens;
+}
--- srcs/Utils/Utils.hpp
+++ srcs/Utils/Utils.hpp
@@ -6,9 +6,10 @@ namespace IRC
 {
     namespace Utils
     {
-            void	checker(int argc);
-			size_t	find_crlf(string &str);
-			string	stringToUpper(const string& str);
-			string	intToString(int num);
+            void				checker(int argc);
+			size_t				find_crlf(string &str);
+			string				stringToUpper(const string& str);
+			string				intToString(int num);
+            std::vector<string>	splitString(const string& s, const string& del);
     };
 }
