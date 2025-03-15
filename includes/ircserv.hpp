571888774954a0eb257deb783fd9d22a3d8d16e4
Thong Zi Qi <ziqi.thong@gamil.com>
2025-03-15 17:49:47 +0800

    feat(update) - numeric replies for join command

--- includes/ircserv.hpp
+++ includes/ircserv.hpp
@@ -28,17 +28,23 @@
 # define MAX_CLIENTS 50
 # define BUFFER_SIZE 512
 // Numeric replies
-# define RPL_WELCOME(client)				(": 001 " + client + " :Welcome to the lala mui zai network " + client) 
+# define RPL_WELCOME(client)							(": 001 " + client + " :Welcome to the lala mui zai network " + client)
+# define RPL_NOTOPIC(client, channel)					(": 331 " + client + " " + channel + " :No topic is set")
+# define RPL_TOPIC(client, channel, topic)				(": 332 " + client + " " + channel + " :" topic)
+# define RPL_TOPICWHOTIME(client, channel, nick, setat)	(": 333 " + client + " " + channel + " " + nick + " " + setat)
+# define RPL_NAMREPLY(client, symbol, channel, prefix_nick_pairs) \
+														(": 353 " + client + " " + symbol + " " + channel + " :" + prefix_nick_pairs)
+# define RPL_ENDOFNAMES(client, channel)				(": 366 " + client + " " + channel + " :End of /NAMES list")
 
 // Error replies
-# define ERR_UNKNOWNCOMMAND(client, command)(": 421 " + client + " " + command + " :Unknown command")
-# define ERR_NONICKNAMEGIVEN(client)		(": 431 " + client + " :No nickname given")
-# define ERR_ERRONEUSNICKNAME(client, nick) (": 432 " + client + nick + ":Erroneus nickname")
-# define ERR_NICKNAMEINUSE(client, nick)	(": 433 " + client + " " + nick + " :Nickname is already in use")
-# define ERR_NOTREGISTERED(client) 			(": 451 " + client + " :You have not registered")
-# define ERR_NEEDMOREPARAMS(client, command)(": 461 " + client + " " + command + " :Not enough paramaters")
-# define ERR_ALREADYREGISTERED(client)		(": 462 " + client +  " :You may not reregister")
-# define ERR_PASSWDMISMATCH(client)			(": 464 " + client + " :Password incorrect")
+# define ERR_UNKNOWNCOMMAND(client, command)			(": 421 " + client + " " + command + " :Unknown command")
+# define ERR_NONICKNAMEGIVEN(client)					(": 431 " + client + " :No nickname given")
+# define ERR_ERRONEUSNICKNAME(client, nick) 			(": 432 " + client + nick + ":Erroneus nickname")
+# define ERR_NICKNAMEINUSE(client, nick)				(": 433 " + client + " " + nick + " :Nickname is already in use")
+# define ERR_NOTREGISTERED(client) 						(": 451 " + client + " :You have not registered")
+# define ERR_NEEDMOREPARAMS(client, command)			(": 461 " + client + " " + command + " :Not enough paramaters")
+# define ERR_ALREADYREGISTERED(client)					(": 462 " + client +  " :You may not reregister")
+# define ERR_PASSWDMISMATCH(client)						(": 464 " + client + " :Password incorrect")
 //--------------------namespace-------------------------//
 using std::cout;
 using std::endl;
