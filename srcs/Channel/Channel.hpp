b67177bb3473b788448434a74d4cfcb4fa8e4209
Thong Zi Qi <ziqi.thong@gamil.com>
2025-03-30 05:25:42 +0800

    Reapply "chore - append properly"
    This reverts commit bd3d35faa38f6ab28d0b4228e01b47510294b297.

--- srcs/Channel/Channel.hpp
+++ srcs/Channel/Channel.hpp
@@ -11,23 +11,23 @@ namespace IRC
     class Channel
     {
         private:
-		string							_channel_name;
-		string							_channel_modes;
-		std::vector<string>				_invite_list;
-		std::vector<IRC::Client *>		_operators;
-		std::map<string, IRC::Client*>	_clients; /**<client_fd, Client*> */
-		string							_ban_list;
-		string							_exception_list;
-		string							_invite_exception_list;
-		string							_topic;
-		string							_topicSetter;
-		time_t							_topicSetTime;
-		string							_key;
+			string								_channel_name;
+			string								_channel_modes;
+			std::vector<string>					_invite_list;
+			std::vector<IRC::Client *>			_operators;
+			std::map<string, IRC::Client*>		_clients; /**<client_fd, Client*> */
+			string								_ban_list;
+			string								_exception_list;
+			string								_invite_exception_list;
+			string								_topic;
+			string								_topicSetter;
+			time_t								_topicSetTime;
+			string								_key;
 
-		void					_handleBanMode(char action, const string &args, IRC::Client &client);
-		void					_handleKeyMode(char action, const string &args, IRC::Client &client);
-		void					_handleExceptionMode(char action, const string &args, IRC::Client &client);
-		void					_handleInviteExceptionMode(char action, const string &args, IRC::Client &client);
+			void								_handleBanMode(char action, const string &args, IRC::Client &client);
+			void								_handleKeyMode(char action, const string &args, IRC::Client &client);
+			void								_handleExceptionMode(char action, const string &args, IRC::Client &client);
+			void								_handleInviteExceptionMode(char action, const string &args, IRC::Client &client);
 
         public:
             Channel();
@@ -36,17 +36,17 @@ namespace IRC
             Channel &operator=(const Channel &other);
             Channel(const string channel_name, IRC::Client& client);
 
-            void							attach(IRC::Client* client);
-			int								detach(IRC::Client* client);
-			void							kickUsers(IRC::Client& client, const string &users, const string &comment);
-			void							notifyAll(const std::string& message, IRC::Client *sender);
-			void							joinNumericReplies(IRC::Client* new_client);
-			bool							isOperator(Client *client);
+            void								attach(IRC::Client* client);
+			int									detach(IRC::Client* client);
+			void								kickUsers(IRC::Client& client, const string &users, const string &comment);
+			void								notifyAll(const std::string& message, IRC::Client *sender);
+			void								joinNumericReplies(IRC::Client* new_client);
+			bool								isOperator(Client *client);
 
 			//Setter(s)
-			void							setChannelName(const string& channel_name);
-			void							setTopic(const string& new_topic, Client &setter);
-			int								setChannelMode(string mode, string args, IRC::Client &client);
+			void								setChannelName(const string& channel_name);
+			void								setTopic(const string& new_topic, Client &setter);
+			int									setChannelMode(string mode, string args, Client &client, Channel &channel);
 
             //Getter(s)
 			const string&						getInviteExceptionList() const;
