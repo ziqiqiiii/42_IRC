c47c6773709b69ee41114c8a27040c1fa9139ee2
ghostyghast <antoinethebestgamer@gmail.com>
2025-04-03 00:04:42 +0800

    feat: channel is now deleted when no users are present on it

--- srcs/Server/ClientChannel.cpp
+++ srcs/Server/ClientChannel.cpp
@@ -58,7 +58,14 @@ int	IRC::Server::leaveChannel(const string& channel_name, Client* client)
 		client->sendResponse(ERR_NOSUCHCHANNEL(client->getNickname(), channel_name));
 		return (0);
 	}
-	return (channel_it->second->detach(client));
+	channel_it->second->detach(client);
+	if (channel_it->second->getClientsList().empty())
+	{
+		delete (channel_it->second);
+		this->_channels.erase(channel_it);
+		return (0);
+	}
+	return (1);
 }
 
 void	IRC::Server::notifyAll(const string& message)
