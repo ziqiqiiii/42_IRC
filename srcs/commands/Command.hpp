73f51fdfaaaf141095c580cc0e48e49e41dcb690
ghostyghast <antoinethebestgamer@gmail.com>
2025-04-09 22:39:25 +0800

    feat: made command constructors and operator= private, and members needed by derived class protected

--- srcs/commands/Command.hpp
+++ srcs/commands/Command.hpp
@@ -14,21 +14,22 @@ namespace IRC {
 			
 			virtual void	execute(std::vector<string> &args, IRC::Client& issuer);
 		
-		private:
-			Server& 				_server;
-			Client const*			_issuer;
-			const string			_name;
-			const int				_min_args;
+		protected:
+			Server& 		_server;
+			Client*			_issuer;
+			const string	_name;
+			const int		_min_args;
 		
-			Command();
-			Command(const Command &copy);
-			Command &operator=(const Command &copy);
 			//Getters
 			IRC::Client*	_getClient(const string& name);
 			Channel*		_getChannel(const string& name);
-
+			
 			virtual void	_validate() = 0;
 			virtual void	_operate() = 0;
 			virtual void	_parse(std::vector<string>& args) = 0;
-	};
+		private:
+			Command();
+			Command(const Command &copy);
+			Command &operator=(const Command &copy);
+		};
 }
